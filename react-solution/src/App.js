dc0ce15030471a0ca2cb0a02062744eabe9ff92a
rick1e <alrickab@gmail.com>
2022-01-21 21:11:20 -0500

    Finial adjustments

--- react-solution/src/App.js
+++ react-solution/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import equilibrium from './assets/image-equilibrium.jpg';
+import avatar from './assets/image-avatar.png';
+import clock from './assets/icon-clock.svg';
+import ethereum from './assets/icon-ethereum.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainCard">
+        <div className="cardImage">
+          <img src={equilibrium} className="card-image" alt="equilibrium" />
+        </div>
+
+        <div className="cardTitle">
+          <span>
+            Equilibrium #3429
+          </span>
+        </div>
+
+        <div className="cardBody">
+          <span>
+            Our Equilibrium collection promotes balance and calm.
+          </span>
+        </div>
+
+        <div className="cardData">
+          <div className="cardPrice">
+            <img src={ethereum} className="ethereum-icon" alt="ethereum" />
+            <span>
+              0.041 ETH
+          </span>
+          </div>
+          <div className="cardTime">
+            <img src={clock} className="clock-icon" alt="clock" />
+            <span>
+              3 days left
+          </span>
+          </div>
+        </div>
+
+        <div className="cardFooter">
+          <img src={avatar} className="avatar-image" alt="avatar" />
+          <span>
+            Creation of <strong>Jules Wyvern</strong>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
